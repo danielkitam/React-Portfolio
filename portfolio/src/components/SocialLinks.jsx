5cb74232e4cc98a78293ea26b7439d2457d381b4
Daniel kitamenywa <danielkitamen@gmail.com>
2023-03-01 16:09:00 +0300

    something changed

--- portfolio/src/components/SocialLinks.jsx
+++ portfolio/src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
                     Resume <BsFillPersonLinesFill size={30} />
                 </>
             ),
-            href: '/don.pdf',
+            href: '',
             style: 'rounded-br-md',
             download: true,
         },
